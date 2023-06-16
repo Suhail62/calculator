7862c91f415853e80aefdbf7720947f42bf7e937
Suhail62 <suhailmuhammed630@gmail.com>
2023-06-16 19:52:51 +0530

    my calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+let string = "";
+let buttons = document.querySelectorAll("button")
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        // show result when user clicked =
+        if(e.target.innerHTML =="="){
+            string = eval(string);
+            document.querySelector("textarea").innerText = string;
+        
+        }
+        // to clear calculations
+        else if(e.target.innerHTML == "AC"){
+        string = "";
+        document.querySelector("textarea").innerText = string;
+    }
+    // to undo last digit
+        else if(e.target.innerHTML == "←"){
+        string = string.slice(0,-1)
+        document.querySelector("textarea").innerText = string;
+        }
+        else{
+            string = string + e.target.innerText;
+            document.querySelector("textarea").innerText = string;
+        }
+    })
+})
